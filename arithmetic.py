70d0d7cbd46c10ac652721b6c52ca080c8439d61
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 12:52:23 -0700

    made more dry, cleaned up

--- arithmetic.py
+++ arithmetic.py
@@ -3,43 +3,49 @@
 
 def add(num1, num2):
     """Return the sum of the two input integers."""
-    total = num1 + num2
-    return total
+
+    return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    total = num1 - num2
-    return total
+
+    return num1 - num2
+
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-    total = num1 * num2
-    return total
+
+    return num1 * num2
+
 
 def divide(num1, num2):
     """Divide the first input by the second, returning a floating point."""
-    total = num1 / num2
-    return total
+
+    return num1 / num2
+
 
 def square(num1):
     """Return the square of the input."""
-    total = num1 ** num1
-    return total
+
+    return num1 ** num1
+
 
 def cube(num1):
     """Return the cube of the input."""
-    total = num1 * num1 * num1
-    return total
+
+    return num1 * num1 * num1
+
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-    total = num1 ** num2
-    return total
+
+    return num1 ** num2
+
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
-    total = num1 % num2
-    return total
+
+    return num1 % num2
 
 #git initialized
